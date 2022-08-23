503bd6798d7f55c0fe30c794b645af42cd8b7ee6
Suriya Kandaswamy <suriya.kandaswamy@gmail.com>
2022-08-23 13:45:42 -0400

    css formatting

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { LoginService } from './services/login.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'amaranth_ringup';
+
+  loggedIn: boolean = false;
+
+  constructor(
+    private loginService: LoginService
+  ) {}
+
+  ngOnInit() {
+    this.loginService.getIsLoggedIn()
+       .subscribe(
+         loggedIn => {
+           this.loggedIn = loggedIn;
+         }
+       )
+  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { HttpClientModule } from '@angular/common/http';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
@@ -38,6 +39,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatAutocompleteModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
