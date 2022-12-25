1878767452e7667f3df02142dbab0f86709aa89c
Indhu Balasubramaniam <indhupriya@gmail.com>
2022-12-25 11:24:34 -0500

    ringup css formatting

--- frontend/src/app/ringup/ringup.component.ts
+++ frontend/src/app/ringup/ringup.component.ts
@@ -2,10 +2,9 @@ import { Component, OnInit, Input } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 
 import { Product } from '../classes/product';
-
 import { ProductService } from '../services/product.service'
-
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { FormControl, FormGroup } from '@angular/forms';
 
 interface RingupProduct {
   product: Product;
@@ -19,6 +18,12 @@ interface RingupProduct {
 })
 export class RingupComponent implements OnInit {
 
+
+  // ringupForm = new FormGroup({
+  //   product : new FormControl(""),
+  //   quantity :  new FormControl(),
+  // });
+
   faTimes = faTimes;
 
   columnsToDisplay = ['name', 'unit_price', 'quantity', 'delete'];
@@ -28,6 +33,10 @@ export class RingupComponent implements OnInit {
   ringupProducts : RingupProduct[] = [];
 
   selectedNewProduct : Product = new Product(-1, "", 0, "lb");
+
+  productquantity : number = 0.0;
+  
+
 
   constructor(
     private productService: ProductService
@@ -62,11 +71,15 @@ export class RingupComponent implements OnInit {
   addRingupProduct() : void {
     var newRingupProduct: RingupProduct = {
       product: this.selectedNewProduct,
-      quantity: 0
+      quantity: this.productquantity,
     };
 
     this.ringupProducts.push(newRingupProduct);
     this.tableDataSource.data = this.ringupProducts;
+
+    this.productquantity = 0;
+    this.selectedNewProduct =  new Product(-1, "", 0, "lb");
+
   }
 
   deleteRingupProduct(ringupProduct: RingupProduct) : void {
